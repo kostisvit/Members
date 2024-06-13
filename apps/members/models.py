5efc1529cc184a58c7cafb1f26845c98e4d104ec
Konstantinos Vitiniotis <kostasvit@gmail.com>
2024-06-13 10:48:43 +0300

    members model

--- apps/members/models.py
+++ apps/members/models.py
@@ -1,3 +1,50 @@
+import random
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Member(models.Model):
+    class Gender(models.TextChoices):
+        MALE = 'M', _('Male')
+        FEMALE = 'F', _('Female')
+        OTHER = 'O', _('Other')
+    
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_of_birth = models.DateField()
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=100)
+    gender = models.CharField(
+        max_length=1,
+        choices=Gender.choices,
+        default=Gender.OTHER,
+    )
+    membership_date = models.DateField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+    membership_number = models.CharField(max_length=10, unique=True, blank=True)
+
+    
+    def save(self, *args, **kwargs):
+        if not self.membership_number:
+            self.membership_number = self.generate_unique_code()
+        super().save(*args, **kwargs)
+
+    def generate_unique_code(self):
+        while True:
+            membership_number = ''.join([str(random.randint(0, 9)) for _ in range(9)])
+            if not Member.objects.filter(membership_number=membership_number).exists():
+                break
+        return membership_number
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+        verbose_name = _('Member')
+        verbose_name_plural = _('Members')
