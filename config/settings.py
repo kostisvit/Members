a82611ac59ec04653db8b9e5da53a3279c1d15ca
Konstantinos Vitiniotis <kostasvit@gmail.com>
2024-06-20 11:01:58 +0300

    staticroot

--- config/settings.py
+++ config/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 STATIC_URL = '/static/'
+STATIC_ROOT = "/staticfiles/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
